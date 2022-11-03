b3eaaef62fa22198e7961df0f90985b60ae17c8f
Eugene Kruglov <sickjahfar@gmail.com>
2022-11-03 14:12:50 +0100

    [emacs] Use https in melpa urls

--- emacs/lisp/init-melpa.el
+++ emacs/lisp/init-melpa.el
@@ -1,4 +1,4 @@
-;;; init-melpa.el --- Melpa init file.
+;; init-melpa.el --- Melpa init file.
 
 ;;; Commentary:
 
@@ -8,9 +8,9 @@
 
 (require 'package)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (package-initialize)
 
